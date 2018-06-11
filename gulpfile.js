c89fe4fb6c8ab42acab2268c854fa32d4aba4cf4
Shane Austin <shane@austindesignco.com>
2018-06-11 12:16:58 -0700

    chore: Created initial package manager and build files

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+var swPrecache = require('sw-precache');
+
+gulp.task('generate-sw', function() {
+  var swOptions = {
+    staticFileGlobs: [
+      './index.html',
+      './images/*.{png,svg,gif,jpg}',
+      './js/*.js',
+      './css/*.css'
+    ],
+    stripPrefix: '.'
+  };
+  return swPrecache.write('./service-worker.js', swOptions);
+});
+
+gulp.task('serve', ['generate-sw'], function() {
+  browserSync.init({
+    notify: false,
+    logPrefix: 'restaurantApp',
+    server: ['.'],
+    port: 8000
+  });
+  gulp.watch([
+    './*.html',
+    './js/*.js',
+    './css/*.css',
+    '!./service-worker.js',
+    '!./gulpfile.js'
+  ], ['generate-sw'], browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
