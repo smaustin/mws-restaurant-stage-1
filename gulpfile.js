2851c1ae761ad01b8ba555b93b826c55f31bc2a1
Shane Austin <shane@austindesignco.com>
2018-08-01 14:32:24 -0700

    chore: Add initial configs and tasks to use indexDB

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
-var responsive = require('gulp-responsive-images');
-// var plugins = require('gulp-load-plugins')({
-//   rename: {
-//     'gulp-responsive-images': 'responsive'
-//   }
-// });
+//var responsive = require('gulp-responsive-images');
+var plugins = require('gulp-load-plugins')({
+  rename: {
+    'gulp-responsive-images': 'responsive'
+  }
+});
 var del = require('del');
 var browserSync = require('browser-sync').create();
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
 //TODO: may need prefixes for flexbox
 //var autoprefixer = require('gulp-autoprefixer');
 // TODO: may want to use sw-precache in future
@@ -25,10 +29,19 @@ var browserSync = require('browser-sync').create();
 //   return swPrecache.write('./service-worker.js', swOptions);
 // });
 
+gulp.task('sw', () => {
+  return browserify({ debug: true })
+    .transform('babelify', { presets: ['env'] })
+    .require('sw.js', { entry: true })
+    .bundle()
+    .pipe(source('sw.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('images', function() {
   // create thumbs
   gulp.src('img/*.jpg')
-    .pipe(responsive({
+    .pipe(plugins.responsive({
       '!empty-plate.jpg': [{
         width: 375,
         suffix: '_1x',
@@ -42,7 +55,7 @@ gulp.task('images', function() {
     .pipe(gulp.dest('img/thumbs'));
   // create banners
   gulp.src('img/*.jpg')  
-    .pipe(responsive({
+    .pipe(plugins.responsive({
       '!empty-plate.jpg': [{
         // height: 400,
         // crop: true,
--- sw.js
+++ sw.js
@@ -1,24 +1,32 @@
-var STATIC_CACHE = 'app-static-v6';
-var IMG_CACHE = 'app-content-imgs'; //TODO
-var ALL_CACHES = [
+import idb from 'idb';
+
+// TODO: append hash version number to STATIC_CACHE and static files to cache
+
+const STATIC_CACHE = 'app-static-v1';
+const DYNAMIC_CACHE = 'app-dynamic';
+const ALL_CACHES = [
 	STATIC_CACHE,
-	IMG_CACHE
+	DYNAMIC_CACHE
 ];
- 
+
+const dbPromise = idb.open('keyval-store', 1, updateDB => {
+	updateDB.createObjectStore('keyval');
+});
+
+// On Service Worker install cache static assets
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open(STATIC_CACHE).then( cache => {
 			return cache.addAll([
 				'/',
-				"/index.html",
-				"/restaurant.html",
-				"/css/styles.css",
-				//"/data/restaurants.json",
-				"/img/empty-plate.jpg",
-				"/js/dbhelper.js",
-				"/js/main.js",
-				"/js/register.js",
-				"/js/restaurant_info.js"
+				'/index.html',
+				'/restaurant.html',
+				'/css/styles.css',
+				'/img/empty-plate.jpg',
+				'/js/dbhelper.js',
+				'/js/main.js',
+				'/js/register.js',
+				'/js/restaurant_info.js'
 			]);
 		}).catch( error => {
 			console.log(error);
@@ -26,12 +34,13 @@ self.addEventListener('install', event => {
 	);
 });
 
+// On Service Worker activation clear the previous static cache versions
 self.addEventListener('activate', event => {
 	event.waitUntil(
 		caches.keys().then(cacheNames => {
 			return Promise.all(
 				cacheNames.filter(cacheName => {
-					return cacheName.startsWith('app-static') && !ALL_CACHES.includes(cacheName);
+					return cacheName.startsWith('app-') && !ALL_CACHES.includes(cacheName);
 				}).map(cacheName => {
 					return caches.delete(cacheName);
 				})
@@ -41,13 +50,30 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
+	const requestURL = new URL(event.request.url);
+	if (requestURL.origin === location.origin) {
+		if (requestURL.pathname === '/') {
+			event.respondWith(caches.match('index.html'));
+			return;
+		} 
+		// restaurant.html will handle all restaurant requests (process url id's)
+		if (requestURL.pathname.startsWith('/restaurant.html')) {
+			event.respondWith(caches.match('restaurant.html'));
+			return;
+		}
+		/*if (requestURL.port === '1337') {
+			// Don't handle API calls
+			return;
+		}*/
+	}
+
 	event.respondWith(
-		caches.match(event.request, {ignoreSearch: true})
+		caches.match(event.request)
 		.then(response => {
 			return response || fetch(event.request)
 				.then(handleFetchErrors)
 				.then(fetchResponse => {
-					return caches.open(STATIC_CACHE)
+					return caches.open(DYNAMIC_CACHE)
 				.then(cache => {
 					cache.put(event.request, fetchResponse);
 					return fetchResponse.clone();
