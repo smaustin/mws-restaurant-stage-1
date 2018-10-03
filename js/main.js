c9df07ab9c8b1741c45ba7b853ca5e374bd9431a
Shane Austin <shane@austindesignco.com>
2018-10-03 06:33:46 -0700

    style: Add semicolons to variables at top of file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let restaurants,
   neighborhoods,
-  cuisines
-var newMap
-var markers = []
+  cuisines;
+var newMap;
+var markers = [];
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
